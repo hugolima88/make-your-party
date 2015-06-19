547b4da3a09fbde1ea58bd4da9876a4faf2f6757
hugolima88 <hugolima88@gmail.com>
2015-06-19 17:44:23 -0400

    Sync with github

---
+++ MakeYourPartyServer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MakeYourPartyServer.Startup))]
+namespace MakeYourPartyServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
